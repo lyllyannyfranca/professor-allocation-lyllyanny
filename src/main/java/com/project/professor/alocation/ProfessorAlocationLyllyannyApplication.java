aae0b764a94a627fe59f5ac8c4a87a6af18312b9
Lyllyanny França <lyllyanny.franca@gmail.com>
2024-02-24 14:29:29 -0300

    add project

---
+++ src/main/java/com/project/professor/alocation/ProfessorAlocationLyllyannyApplication.java
@@ -0,0 +1,13 @@
+package com.project.professor.alocation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProfessorAlocationLyllyannyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProfessorAlocationLyllyannyApplication.class, args);
+	}
+
+}
